f2a8ec441951d1ebb887926a55d02b831cc6e847
Juan Manuel Garcia <jmgarcia@irpaglide.com>
2020-01-20 10:15:19 -0300

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "private_key_pem" {
- count = "${var.enabled}"
 value = "${acme_certificate.cert.private_key_pem}"
+value = "${local.private_key}"
 }
 
 output "certificate_pem" {
-count = "${var.enabled}"
 value = "${acme_certificate.cert.certificate_pem}"
+value = "${local.certificate}"
 }
 
 output "issuer_pem" {
-count = "${var.enabled}"
 value = "${acme_certificate.cert.issuer_pem}"
+value = "${local.issuer}"
 }
 
 output "certificate_arn" {
-count = "${var.enabled}"
 value = "${aws_iam_server_certificate.ssl_cert.arn}"
+value = "${local.certificate_arn}"
 }
