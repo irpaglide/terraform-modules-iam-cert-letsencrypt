d1e62470140fe88639689d65871b81beb0e6edbe
Juan Manuel Garcia <jmgarcia@irpaglide.com>
2020-01-20 10:55:12 -0300

    fix locals

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
-	private_key = "${enabled == true ? acme_certificate.cert.private_key_pem : "disabled"}"
-	certificate = "${enabled == true ? acme_certificate.cert.certificate_pem : "disabled"}"
-	issuer = "${enabled == true ? acme_certificate.cert.issuer_pem : "disabled" }"
-	certificate_arn = "${enabled == true ? aws_iam_server_certificate.ssl_cert.arn : "disabled" }"
+	private_key = "${var.enabled == true ? acme_certificate.cert.private_key_pem : "disabled"}"
+	certificate = "${var.enabled == true ? acme_certificate.cert.certificate_pem : "disabled"}"
+	issuer = "${var.enabled == true ? acme_certificate.cert.issuer_pem : "disabled" }"
+	certificate_arn = "${var.enabled == true ? aws_iam_server_certificate.ssl_cert.arn : "disabled" }"
 }
