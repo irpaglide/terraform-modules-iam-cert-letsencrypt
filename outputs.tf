d364f056d6abaece69d7f4dabaa30a5173dc2935
Juan Manuel Garcia <jmgarcia@irpaglide.com>
2020-01-20 10:20:20 -0300

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,15 @@
 output "private_key_pem" {
-value = "${acme_certificate.cert.private_key_pem}"
 value = "${local.private_key}"
 }
 
 output "certificate_pem" {
-value = "${acme_certificate.cert.certificate_pem}"
 value = "${local.certificate}"
 }
 
 output "issuer_pem" {
-value = "${acme_certificate.cert.issuer_pem}"
 value = "${local.issuer}"
 }
 
 output "certificate_arn" {
-value = "${aws_iam_server_certificate.ssl_cert.arn}"
 value = "${local.certificate_arn}"
 }
